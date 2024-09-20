acff18c28fa8076471a93eea14c936400bf37838
Your Name <you@example.com>
2024-09-20 10:34:12 +0200

    extended

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // applyBaseStyles: false,
-  })],
+  integrations: [
+    tailwind({
+      // applyBaseStyles: false,
+    }),
+  ],
 
-  output: 'server',
+  output: "server",
 
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
