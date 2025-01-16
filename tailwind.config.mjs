7a75f2ae0fee8fa825a409d339d11b97318637d7
TeraBasedProgrammer <ilyadronov2332@gmail.com>
2025-01-16 13:18:58 +0200

    fix: fix ion explore / analyze swap animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'custom-lg': '1400px'
+      }
+    },
   },
   plugins: [],
 };
