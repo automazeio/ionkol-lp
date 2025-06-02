1c3027d76cccc0aedb750c9adf5deaa7e1a39beb
Maksym Bakovetskyi <bakoveckiymax@gmail.com>
2025-06-02 16:04:25 +0300

    feat: image animation, new swiper

--- src/components/react/PriceCards.tsx
+++ src/components/react/PriceCards.tsx
@@ -3,6 +3,10 @@ import type { PriceCard } from "../../../lib/directus";
 import directus from "../../../lib/directus";
 import { readItems } from "@directus/sdk";
 import { m } from "../../paraglide/messages";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function PriceCardsData() {
   const [data, setData] = useState<PriceCard[]>([]);
@@ -22,44 +26,104 @@ export default function PriceCardsData() {
 
   return (
     <>
-      {data.length > 0 ? (
-        data.map((price) => (
-          <div className="bg-[#FFE2E5] rounded-[220px] px-8 py-12 flex flex-col items-center text-center min-w-[318px] min-h-[612px]">
-            <h3 className="text-[34px] font-semibold mb-10">{price.name}</h3>
-            {price.name === "Free" ? (
-              <p className="free-tier text-[68px] font-bold mb-[44px]">Free</p>
-            ) : (
-              <>
-                <div className="price-container">
-                  <p className="monthly-price text-[68px] font-bold">
-                    {price.price_monthly}
-                    <span className="text-3xl">USD</span>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 w-full mt-14">
+        {data.length > 0 ? (
+          data.map((price) => (
+            <div
+              key={price.id}
+              className="bg-[#FFE2E5] rounded-[220px] px-8 py-12 flex flex-col items-center text-center min-w-[318px] min-h-[612px]"
+            >
+              <h3 className="text-[34px] font-semibold mb-10">{price.name}</h3>
+              {price.name === "Free" ? (
+                <p className="free-tier text-[68px] font-bold mb-[44px]">
+                  Free
+                </p>
+              ) : (
+                <>
+                  <div className="price-container">
+                    <p className="monthly-price text-[68px] font-bold">
+                      {price.price_monthly}
+                      <span className="text-3xl">USD</span>
+                    </p>
+                    <p className="annual-price text-[68px] font-bold hidden">
+                      {price.price_monthly_annual}
+                      <span className="text-3xl">USD</span>
+                    </p>
+                  </div>
+                  <p className="text-xl mb-4">
+                    {m["index.pricing.perMonth"]()}
                   </p>
-                  <p className="annual-price text-[68px] font-bold hidden">
-                    {price.price_monthly_annual}
-                    <span className="text-3xl">USD</span>
-                  </p>
-                </div>
-                <p className="text-xl mb-4">{m["index.pricing.perMonth"]()}</p>
-                <div className="yearly-price text-xl mb-6 flex flex-col hidden">
-                  <span>$ {price.price_yearly}</span>
-                  <span>({m["index.pricing.billedAnnually"]()})</span>
-                </div>
-              </>
-            )}
+                  <div className="yearly-price text-xl mb-6 flex flex-col hidden">
+                    <span>$ {price.price_yearly}</span>
+                    <span>({m["index.pricing.billedAnnually"]()})</span>
+                  </div>
+                </>
+              )}
 
-            <p className="text-xl mb-8">{price.subheader}</p>
-            <div className="w-full h-[1px] bg-black/30 mb-8"></div>
-            <ul className="space-y-2 text-center w-full text-[20px]">
-              {price.description.map((line) => (
-                <li>{line.replace(".", ",")}</li>
-              ))}
-            </ul>
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+              <p className="text-xl mb-8">{price.subheader}</p>
+              <div className="w-full h-[1px] bg-black/30 mb-8"></div>
+              <ul className="space-y-2 text-center w-full text-[20px]">
+                {price.description.map((line) => (
+                  <li key={line}>{line.replace(".", ",")}</li>
+                ))}
+              </ul>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+
+      {/* {data.length > 0 && (
+        <Swiper
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          modules={[Pagination]}
+          className="lg:hidden"
+        >
+          {data.map((price) => (
+            <SwiperSlide
+              key={price.id}
+              className="bg-[#FFE2E5] rounded-[220px] px-8 py-12 flex flex-col items-center text-center w-[318px] min-h-[612px]"
+            >
+              <h3 className="text-[34px] font-semibold mb-10">{price.name}</h3>
+              {price.name === "Free" ? (
+                <p className="free-tier text-[68px] font-bold mb-[44px]">
+                  Free
+                </p>
+              ) : (
+                <>
+                  <div className="price-container">
+                    <p className="monthly-price text-[68px] font-bold">
+                      {price.price_monthly}
+                      <span className="text-3xl">USD</span>
+                    </p>
+                    <p className="annual-price text-[68px] font-bold hidden">
+                      {price.price_monthly_annual}
+                      <span className="text-3xl">USD</span>
+                    </p>
+                  </div>
+                  <p className="text-xl mb-4">
+                    {m["index.pricing.perMonth"]()}
+                  </p>
+                  <div className="yearly-price text-xl mb-6 flex flex-col hidden">
+                    <span>$ {price.price_yearly}</span>
+                    <span>({m["index.pricing.billedAnnually"]()})</span>
+                  </div>
+                </>
+              )}
+
+              <p className="text-xl mb-8">{price.subheader}</p>
+              <div className="w-full h-[1px] bg-black/30 mb-8"></div>
+              <ul className="space-y-2 text-center w-full text-[20px]">
+                {price.description.map((line) => (
+                  <li>{line.replace(".", ",")}</li>
+                ))}
+              </ul>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )} */}
     </>
   );
 }
